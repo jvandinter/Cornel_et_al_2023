05aed5c5c0546db31d30b1f823e4457e4f3d38b6
jvandinter <jipvandinter@hotmail.nl>
2023-06-21 15:39:20 +0200

    initial commit

---
+++ 01_wrangle_quant_data.R
@@ -0,0 +1,118 @@
+# Recreation of Figure for Annelisa
+library(tidyverse)
+
+project_dir = "/hpc/pmc_vanheesch/projects/Jip/Annelisa_counts"
+
+# Read cohort metadata
+annelisa_meta <-
+  read.delim(paste(project_dir, "Annelisa_cohort.txt", sep = "/"))
+
+# Grab MR1 count info
+mr1l <- pbapply::pbapply(annelisa_meta, 1, function(x) {
+  mr1 <-
+    data.table::fread(paste(project_dir, x[1], sep = "/"),
+                      sep = "\t",
+                      skip = 3)[c(4094, 19125, 19222, 19223),]
+  mr1$Biomaterial.Id <- x[3]
+  return(mr1)
+})
+
+# Combine in single DF
+mr1 <- do.call(plyr::rbind.fill, mr1l)
+
+# Some files have a different annotation for MR1, find those, subset the metadata
+# and rerun those with the correct location
+table(mr1$GeneName %in% c("MR1", "HLA-A", "HLA-B", "HLA-C"))
+to_rerun <-
+  subset(annelisa_meta, Biomaterial.Id %in% mr1[which(!(mr1$GeneName %in% c("MR1", "HLA-A", "HLA-B", "HLA-C"))),]$Biomaterial.Id)
+mr1_correct <-
+  mr1[which(mr1$GeneName %in% c("MR1", "HLA-A", "HLA-B", "HLA-C")),]
+
+test <- data.table::fread(
+  paste(project_dir,
+        to_rerun$quant_files[1],
+        sep = "/"),
+  sep = "\t",
+  skip = 3
+)
+
+# Rerun old version samples
+mr1l <- pbapply::pbapply(to_rerun, 1, function(x) {
+  mr1 <-
+    data.table::fread(paste(project_dir, x[1], sep = "/"),
+                      sep = "\t",
+                      skip = 3)[c(4016, 18414, 18511, 18510),]
+  mr1$Biomaterial.Id <- x[3]
+  return(mr1)
+})
+
+# Make sure they have the same columns
+mr1_rerun <- do.call(plyr::rbind.fill, mr1l)
+mr1_rerun$`# ID` <- NA
+
+mr1_full <- rbind(mr1_correct, mr1_rerun)
+
+# Subset on interesting columns
+mr1_full <-
+  mr1_full[, c("Counts", "CPM", "FPKM", "GeneID", "GeneName", "Biomaterial.Id")]
+
+# Connect to interesting metadata
+plotdata <-
+  dplyr::left_join(mr1_full, annelisa_meta[, c("Biomaterial.Id", "V2")])
+unique(plotdata$V2)
+
+# Edit sample labels for plotting
+plotdata$V2 <- ifelse(
+  plotdata$V2 == "Hepatoblastoma",
+  "HBL",
+  ifelse(
+    plotdata$V2 == "Medulloblastoma",
+    "MB",
+    ifelse(plotdata$V2 == "Ependymoma", "EPN", plotdata$V2)
+  )
+)
+plotdata$embryonal <-
+  ifelse(plotdata$V2 %in% c("NBL", "MRT", "HBL", "ATRT", "MB"),
+         "Embryonal",
+         "Not Embryonal")
+
+plotdata_wide <-
+  data.frame(Biomaterial.Id = unique(plotdata$Biomaterial.Id)) %>%
+  dplyr::left_join(plotdata[, 6:8]) %>%
+  dplyr::distinct(Biomaterial.Id, GeneName, .keep_all = TRUE) %>%
+  dplyr::mutate(
+    MR1 =
+      plotdata[which(plotdata$GeneName == "MR1"), c("CPM")],
+    HLA_A =
+      plotdata[which(plotdata$GeneName == "HLA-A"), c("CPM")],
+    HLA_B =
+      plotdata[which(plotdata$GeneName == "HLA-B"), c("CPM")],
+    HLA_C =
+      plotdata[which(plotdata$GeneName == "HLA-C"), c("CPM")]
+  )
+
+# Create list of samples
+write.table(
+  as.data.frame(table(plotdata$V2)),
+  file = paste(project_dir, "samples_per_type.txt", sep = "/"),
+  row.names = F,
+  col.names = F,
+  quote = F,
+  sep = "\t"
+)
+
+write.table(
+  plotdata,
+  file = paste(project_dir, "MR1_plotdata.txt", sep = "/"),
+  row.names = F,
+  quote = F,
+  sep = "\t"
+)
+
+write.table(
+  plotdata_wide,
+  file = paste(project_dir, "mr1_hla_plotdata.txt", sep = "/"),
+  row.names = F,
+  quote = F,
+  sep = "\t"
+)
---
+++ 02_create_figures.R
@@ -0,0 +1,242 @@
+# Recreation of Figure for Annelisa
+library(tidyverse)
+
+project_dir = "/hpc/pmc_vanheesch/projects/Jip/Annelisa_counts"
+
+# Load quantification data
+plotdata_wide <- read.delim(
+  file = paste(project_dir,
+               "mr1_hla_plotdata.txt", sep = "/"),
+  sep = "\t",
+  header = T
+)
+
+# Colour schemes
+embryo_col <- c("#A5CAEB", "#F8B0AF")
+
+# Make the various plots for the figures
+p3 <-
+  ggplot(data = plotdata_wide, aes(
+    x = MR1,
+    y = HLA_A,
+    col = V2,
+    shape = embryonal
+  )) +
+  geom_point() +
+  scale_x_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2",
+                                  scales::math_format(2 ^ .x))
+  ) +
+  scale_y_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2",
+                                  scales::math_format(2 ^ .x))
+  ) +
+  labs(y = "HLA-A expression",
+       x = "MR1 expression",
+       col = "Tumor type",
+       shape = "Tumor class") +
+  scale_shape_manual(values = c(3, 18)) +
+  theme_classic() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_text(size = 10, color = "black"),
+    axis.title = element_text(size = 11, face = "bold"),
+    legend.title = element_text(size = 11, face = "bold"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(colour = "black", size = 1)
+  )
+
+p4 <-
+  ggplot(data = plotdata_wide, aes(
+    x = MR1,
+    y = HLA_B,
+    col = V2,
+    shape = embryonal
+  )) +
+  geom_point() +
+  scale_x_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2", scales::math_format(2 ^ .x))
+  ) +
+  scale_y_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2", scales::math_format(2 ^ .x))
+  ) +
+  labs(y = "HLA-B expression",
+       x = "MR1 expression",
+       col = "Tumor type",
+       shape = "Tumor class") +
+  scale_shape_manual(values = c(3, 18)) +
+  theme_classic() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_text(size = 10, color = "black"),
+    axis.title = element_text(size = 11, face = "bold"),
+    legend.title = element_text(size = 11, face = "bold"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(colour = "black", size = 1)
+  )
+
+p5 <-
+  ggplot(data = plotdata_wide, aes(
+    x = MR1,
+    y = HLA_C,
+    col = V2,
+    shape = embryonal
+  )) +
+  geom_point() +
+  scale_x_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2", scales::math_format(2 ^ .x))
+  ) +
+  scale_y_continuous(
+    trans = 'log2',
+    breaks = scales::trans_breaks("log2", function(x)
+      2 ^ x),
+    labels = scales::trans_format("log2", scales::math_format(2 ^ .x))
+  ) +
+  labs(y = "HLA-C expression",
+       x = "MR1 expression",
+       col = "Tumor type",
+       shape = "Tumor class") +
+  scale_shape_manual(values = c(3, 18)) +
+  theme_classic() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_text(size = 10, color = "black"),
+    axis.title = element_text(size = 11, face = "bold"),
+    legend.title = element_text(size = 11, face = "bold"),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(colour = "black", size = 1)
+  )
+
+p1_violin <-
+  ggplot(data = plotdata, aes(
+    x = reorder(V2, CPM),
+    y = CPM,
+    fill = V2
+  )) +
+  geom_hline(yintercept = 20) +
+  geom_violin(scale = "width") +
+  ggbeeswarm::geom_quasirandom(
+    fill = "white",
+    col = "black",
+    shape = 21,
+    size = 1.2
+  ) +
+  ggpubr::stat_compare_means() +
+  labs(y = "MR1 expression (counts per million)") +
+  
+  theme_classic() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 10),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    legend.position = "none",
+    panel.background = element_rect(colour = "black", size = 1)
+  )
+
+p2 <-
+  ggplot(data = plotdata, aes(x = embryonal, y = CPM, fill = embryonal)) +
+  geom_hline(yintercept = 20) +
+  geom_violin(scale = "width") +
+  ggbeeswarm::geom_quasirandom(
+    fill = "white",
+    col = "black",
+    shape = 21,
+    size = 0.9
+  ) +
+  labs(y = "MR1 expression (counts per million)") +
+  scale_fill_manual(values = embryo_col) +
+  ggpubr::stat_compare_means() +
+  theme_classic() +
+  theme(
+    axis.line = element_blank(),
+    axis.text = element_text(size = 12, color = "black"),
+    axis.title = element_text(size = 10),
+    panel.grid.minor = element_blank(),
+    axis.title.x = element_blank(),
+    legend.position = "none",
+    panel.background = element_rect(colour = "black", size = 1)
+  )
+
+# Correlations presented on the figures
+cor.test(log2(plotdata_wide$MR1), log2(plotdata_wide$HLA_A), method = "pearson")
+cor.test(log2(plotdata_wide$MR1), log2(plotdata_wide$HLA_B), method = "pearson")
+cor.test(log2(plotdata_wide$MR1), log2(plotdata_wide$HLA_C), method = "pearson")
+
+# Save plots
+ggsave(
+  filename = "MR1_CPM_per_tumortype_withpoints.pdf",
+  device = "pdf",
+  plot = p1_violin,
+  height = 6,
+  width = 8,
+  path = project_dir
+)
+
+ggsave(
+  filename = "MR1_CPM_embryonal_withpoints.pdf",
+  device = "pdf",
+  plot = p2,
+  height = 3,
+  width = 4,
+  path = project_dir
+)
+
+ggsave(
+  filename = "HLA-A_CPM_embryonal.pdf",
+  device = "pdf",
+  plot = p3,
+  height = 6,
+  width = 8,
+  path = project_dir
+)
+
+ggsave(
+  filename = "HLA-B_CPM_embryonal.pdf",
+  device = "pdf",
+  plot = p4,
+  height = 6,
+  width = 8,
+  path = project_dir
+)
+ggsave(
+  filename = "HLA-C_CPM_embryonal.pdf",
+  device = "pdf",
+  plot = p5,
+  height = 6,
+  width = 8,
+  path = project_dir
+)
+
+ggsave(
+  filename = "HLA_CPM_per_tumortype.pdf",
+  device = "pdf",
+  plot = ggpubr::ggarrange(
+    p3,
+    p4,
+    p5,
+    ncol = 3,
+    nrow = 1,
+    common.legend = T,
+    legend = "right"
+  ),
+  height = 6,
+  width = 18,
+  path = project_dir
+)
